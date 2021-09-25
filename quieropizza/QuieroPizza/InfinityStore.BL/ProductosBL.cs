3b27f6149027fa42ea0aff43cbfcb922c5cb3590
HeslerErazo <erazohesler@gmail.com>
2021-09-25 16:30:02 -0500

    Agregando Entity Framework parte 1 proyecto

--- quieropizza/QuieroPizza/InfinityStore.BL/ProductosBL.cs
+++ quieropizza/QuieroPizza/InfinityStore.BL/ProductosBL.cs
@@ -8,29 +8,17 @@ namespace InfinityStore.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
        public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pizza 6 quesos";
-            producto1.Precio = 200;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pizza 4 estaciones ";
-            producto2.Precio = 300;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pizza Jamon y Queso";
-            producto3.Precio = 100;
-
-            var listaProductos = new List<Producto>();
-            listaProductos.Add(producto1);
-            listaProductos.Add(producto2);
-            listaProductos.Add(producto3);
-
-            return listaProductos;
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
         }
     }
 }
